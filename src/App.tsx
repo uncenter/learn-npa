2e39c23abed43e34769383e49e0af7fe22af8e95
uncenter <me@uncenter.org>
2023-04-05 11:53:01 -0400

    feat: desktop-only component on mobile (pain)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HopeThemeConfig, HopeProvider } from "@hope-ui/solid";
 import NatoAlphabetQuiz from "./pages/NatoAlphabet";
 import Apps from "./pages/Apps";
 import { Route, Routes } from "@solidjs/router";
+import { createEffect, createSignal } from "solid-js";
 
 const config: HopeThemeConfig = {
     initialColorMode: "system",
@@ -15,14 +16,49 @@ const config: HopeThemeConfig = {
     components: {},
 };
 
+const DesktopOnly = () => (
+    <div class="flex items-center justify-center h-screen mx-4">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold mb-4">
+                This app is meant for desktop only.
+            </h1>
+            <p class="text-lg">Please access it from your desktop computer.</p>
+        </div>
+    </div>
+);
+
 const App: Component = () => {
+    const [isMobile, setIsMobile] = createSignal(false);
+
+    createEffect(() => {
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        const isScreenSmall = window.innerWidth < 768;
+        setIsMobile(isMobile || isScreenSmall);
+
+        const interval = setInterval(() => {
+            const isScreenSmall = window.innerWidth < 768;
+            setIsMobile(isMobile || isScreenSmall);
+        }, 500);
+
+        return () => clearInterval(interval);
+    });
+
     return (
-        <HopeProvider config={config}>
-            <Routes>
-                <Route path="/" component={Apps} />
-                <Route path="/nato-alphabet" component={NatoAlphabetQuiz} />
-            </Routes>
-        </HopeProvider>
+        <>
+            {isMobile() ? (
+                <DesktopOnly />
+            ) : (
+                <HopeProvider config={config}>
+                    <Routes>
+                        <Route path="/" component={Apps} />
+                        <Route
+                            path="/nato-alphabet"
+                            component={NatoAlphabetQuiz}
+                        />
+                    </Routes>
+                </HopeProvider>
+            )}
+        </>
     );
 };
 export default App;
