fd49e79bf56aa467f9faa3344d2c35f79b3ca81b
uncenter <me@uncenter.org>
2023-04-06 23:28:16 -0400

    perf: preload images/assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import VitePluginInjectPreload from "vite-plugin-inject-preload";
 
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-    plugins: [solidPlugin(), ViteImageOptimizer()],
+    plugins: [
+        solidPlugin(),
+        ViteImageOptimizer(),
+        VitePluginInjectPreload({
+            files: [
+                {
+                    match: /.(png|jpg|jpeg|gif|svg|webp|avif|mp4|webm|ogg|mp3|wav|flac|aac|woff|woff2|eot|ttf|otf)/,
+                },
+            ],
+        }),
+    ],
     server: {
         port: 3000,
     },
