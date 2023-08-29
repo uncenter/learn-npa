dad51b12b8888c06f25dee6ce6a5a4e48329b74d
uncenter <47499684+uncenter@users.noreply.github.com>
2023-08-29 12:37:27 -0400

    feat: init shadcn-solid

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import solid from 'solid-start/vite';
-import { defineConfig } from 'vite';
 import vercel from 'solid-start-vercel';
+import path from 'path';
+
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
 	return {
 		plugins: [solid({ ssr: true, adapter: vercel({ edge: false }) })],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+			},
+		},
 	};
 });
