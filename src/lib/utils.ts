d43f60a0e4b4b0faa3d6f28f819a191921297e40
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-02 18:05:10 -0400

    refactor: remove unused utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,6 @@ import { twMerge } from 'tailwind-merge';
 
 export const cn = (...classLists: ClassValue[]) => twMerge(clsx(classLists));
 
-export const mergeArrays = <T>(...arrays: T[][]): T[] => ([] as T[]).concat(...arrays);
-
 export function getRandomItem<T>(arr: T[]): T {
 	return arr[Math.floor(Math.random() * arr.length)];
 }
